664bba481bfb5df42706eaeb4cd7169b06eab321
chronos7447 <35457427+chronos7447@users.noreply.github.com>
2020-10-20 21:30:31 +0530

    Update identity.tf

--- identity.tf
+++ identity.tf
@@ -24,3 +24,10 @@ resource "okta_user_schema" "crn_extension" {
   depends_on = [okta_user_schema.terratest_extension_dev]
 }
 
+data "okta_users" "example" {
+  search {
+    name       = "profile.role"
+    value      = "Testing"
+    comparison = "sw"
+  }
+}
