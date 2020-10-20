112129d69ee939efd2050c58c59cf73edcf5aa8e
chronos7447 <35457427+chronos7447@users.noreply.github.com>
2020-10-20 20:41:40 +0530

    Update mfaPolicy&mfaRuleTest.tf

--- mfaPolicy&mfaRuleTest.tf
+++ mfaPolicy&mfaRuleTest.tf
@@ -1,12 +1,3 @@
-variable "org_name" {}
-variable "api_token" {}
-variable "base_url" {}
-
-provider "okta" {
-    org_name = var.org_name
-    base_url = var.base_url
-    api_token = var.api_token
-}
 
 resource "okta_policy_mfa" "TerraMFAPolicy" {
   name        = "TerraMFAPolicy"
