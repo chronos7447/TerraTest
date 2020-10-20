7a93c196cc9adc36333169249462abbcdcf76395
chronos7447 <35457427+chronos7447@users.noreply.github.com>
2020-10-20 21:41:14 +0530

    Update CreateGroup.tf

--- CreateGroup.tf
+++ CreateGroup.tf
@@ -1,6 +1,13 @@
+data "okta_users" "example" {
+  search {
+    name       = "profile.role"
+    value      = "Testing"
+    comparison = "sw"
+  }
+}
 
 resource "okta_group" "terraCreateGroup" {
   name        = "GroupCreatedUsingTerra"
   description = "My Terra Group"
-  depends_on = [okta_users.example]
+  users = "okta_users.example"
 }
