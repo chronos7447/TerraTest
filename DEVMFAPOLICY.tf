263dcc0180ad7e060211d01f8e9b5d0a330cb24d
chronos7447 <35457427+chronos7447@users.noreply.github.com>
2020-10-20 20:59:14 +0530

    Update DEVMFAPOLICY.tf

--- DEVMFAPOLICY.tf
+++ DEVMFAPOLICY.tf
@@ -10,7 +10,7 @@ resource "okta_policy_mfa" "TerraMFAPolicy" {
   groups_included = ["00gzwvbpeLoYBsD9O4x6"]
 }
 
-resource "okta_policy_mfa_rule" "TerraMFARule" {
+resource "okta_policy_rule_mfa" "TerraMFARule" {
   policyid = "00p17l45g9kcvT3xU4x7"
   name = "TerraMFARule"
   status = "ACTIVE"
