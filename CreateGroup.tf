cf58c037cdd6cd82c7620ffc9677d26074bb68a3
chronos7447 <35457427+chronos7447@users.noreply.github.com>
2020-10-20 22:54:53 +0530

    Create CreateGroup.tf

---
+++ CreateGroup.tf
@@ -0,0 +1,13 @@
+data "okta_users" "example" {
+  search {
+    name       = "profile.role"
+    value      = "Testing"
+    comparison = "sw"
+  }
+}
+    
+resource "okta_group" "terraCreateGroup" {
+  name        = "GroupCreatedUsingTerra"
+  description = "My Terra Group"
+  users = "data.okta_users.example.users"
+}
