85079f35f4e226ba4ca86dc01167165f4c2a20fb
chronos7447 <35457427+chronos7447@users.noreply.github.com>
2020-10-20 20:50:42 +0530

    Create DEVMFAPOLICY.tf

---
+++ DEVMFAPOLICY.tf
@@ -0,0 +1,11 @@
+resource "okta_policy_mfa" "TerraMFAPolicy" {
+  name        = "TerraMFAPolicy"
+  status      = "ACTIVE"
+  description = "TerraMFAPolicyExample"
+
+  okta_otp = {
+    enroll = "REQUIRED"
+  }
+
+  groups_included = ["00gzwvbpeLoYBsD9O4x6"]
+}
