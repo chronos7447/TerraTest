22aa93f12fb7570b27ba97b72a3769a22b8761b7
chronos7447 <35457427+chronos7447@users.noreply.github.com>
2020-10-20 21:10:58 +0530

    Update DEVPASSPOLICY.tf

--- DEVPASSPOLICY.tf
+++ DEVPASSPOLICY.tf
@@ -6,3 +6,10 @@ resource "okta_policy_password" "terraPassPolicy" {
   groups_included        = ["00gzwvbpeLoYBsD9O4x6"]
   sms_recovery           = "ACTIVE"
 }
+
+resource "okta_policy_rule_password" "terraPassPolicyRule" {
+  policyid = "00p17lawagm1iQHvq4x7"
+  name = "terraPassPolicyRule"
+  status = "ACTIVE"
+  password_unlock - "ALLOW"
+}
