198706f656558b6cf9a8c23b0e905b64b031f5d5
chronos7447 <35457427+chronos7447@users.noreply.github.com>
2020-10-20 23:10:09 +0530

    Update CreateGroup.tf

--- CreateGroup.tf
+++ CreateGroup.tf
@@ -9,5 +9,5 @@ data "okta_users" "example" {
 resource "okta_group" "terraCreateGroup" {
   name        = "GroupCreatedUsingTerra"
   description = "My Terra Group"
-  users = [data.okta_users.example.users.1]
+  users = [data.okta_users.example.users.0]
 }
