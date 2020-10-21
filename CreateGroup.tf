d8af8bcaaa4877fa2b29de34fedffed653d75ea8
chronos7447 <35457427+chronos7447@users.noreply.github.com>
2020-10-21 14:55:27 +0530

    Update CreateGroup.tf

--- CreateGroup.tf
+++ CreateGroup.tf
@@ -1,4 +1,18 @@
     
+resource "okta_group" "terraCreateGroup" {
+  name        = "GroupCreatedUsingTerra"
+  description = "My Terra Group"
+}
+
+resource "okta_group_rule" "terraCreateGroupRule" {
+  name              = "terraCreateGroupRule"
+  status            = "ACTIVE"
+  group_assignments = [okta_group.terraCreateGroup.id]
+  expression_type   = "urn:okta:expression:1.0"
+  expression_value  = "String.stringContains(toLowerCase(user.role),\"testing\")"
+}
+
+
 resource "okta_group" "terraCreateGroup01" {
   name        = "GroupCreatedUsingTerra01"
   description = "My Terra Group01"
@@ -11,3 +25,4 @@ resource "okta_group_rule" "terraCreateGroupRule01" {
   expression_type   = "urn:okta:expression:1.0"
   expression_value  = "String.stringContains(toLowerCase(user.role),\"testing\")"
 }
+
