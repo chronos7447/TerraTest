9829353884841bf5abd26617fb8814061b78acf1
chronos7447 <35457427+chronos7447@users.noreply.github.com>
2020-10-23 10:12:11 +0530

    Update AdminSignOnPolicy.tf

--- BaseModelSetup/AdminSignOnPolicy.tf
+++ BaseModelSetup/AdminSignOnPolicy.tf
@@ -12,6 +12,7 @@ resource "okta_policy_rule_signon" "SignOnRule" {
   mfa_required = "true"
   mfa_prompt = "ALWAYS"
   status = "ACTIVE"
+  session_idle = "15"
   session_lifetime = "15" 
 }
 
@@ -19,7 +20,7 @@ resource "okta_policy_rule_signon" "SignOnRuleNetwork" {
   policyid = join("",[okta_policy_signon.SignOn.id])
   name = "Network Zone Restriction Rule "
   network_connection = "OFF_NETWORK"
-  network_includes = ["okta_network_zone.AIG", "okta_network_zone.AIG_Corporate"]
+  #network_includes = ["okta_network_zone.AIG", "okta_network_zone.AIG_Corporate"]
   access = "DENY"
   priority = "1"
 }
