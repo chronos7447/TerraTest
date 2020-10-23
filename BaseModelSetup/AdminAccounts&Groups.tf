86a01e1d7afb74cb7b3c6d9486f9be36ac3c235f
chronos7447 <35457427+chronos7447@users.noreply.github.com>
2020-10-23 12:24:06 +0530

    Update AdminAccounts&Groups.tf

--- BaseModelSetup/AdminAccounts&Groups.tf
+++ BaseModelSetup/AdminAccounts&Groups.tf
@@ -31,3 +31,52 @@ resource "okta_group_roles" "AdminRoletoL3" {
   group_id    = tostring(okta_group.CreateGroup.id)
   admin_roles = ["SUPER_ADMIN"]
 }
+
+
+
+
+
+
+
+################# HELP DESK USERS #####################################
+
+
+resource "okta_user" "User04" {
+  email       = "HelpUser01@mail.com"
+  login       = "HelpUser01@mail.com"
+  first_name        = "Help"
+  last_name = "User01"
+  status    = "ACTIVE"
+  password  = "George01"
+}
+
+resource "okta_user" "User05" {
+  email       = "HelpUser02@mail.com"
+  login       = "HelpUser02@mail.com"
+  first_name        = "Help"
+  last_name = "User02"
+}
+
+resource "okta_user" "User06" {
+  email       = "HelpUser03@mail.com"
+  login       = "HelpUser03@mail.com"
+  first_name        = "Help"
+  last_name = "User03"
+}
+
+resource "okta_group" "CreateGroup01" {
+  name        = "CIAM-HelpDesk"
+  description = "AIG Help Desk Group"
+  users = [okta_user.User04.id, okta_user.User05.id, okta_user.User06.id]
+}
+
+resource "okta_group_roles" "AdminRoletoHelpDesk" {
+  group_id    = tostring(okta_group.CreateGroup01.id)
+  admin_roles = ["SUPER_ADMIN"]
+}
+
+
+
+
+
+
