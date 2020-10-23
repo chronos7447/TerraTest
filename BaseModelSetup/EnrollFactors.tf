c0a21ef7a8b79c6b6ce8a95b55a35232d0308598
chronos7447 <35457427+chronos7447@users.noreply.github.com>
2020-10-23 14:12:42 +0530

    Create EnrollFactors.tf

---
+++ BaseModelSetup/EnrollFactors.tf
@@ -0,0 +1,34 @@
+resource "okta_factor" "factor_enable" {
+  provider_id = "okta_otp"
+  active = "true"
+}
+
+resource "okta_factor" "factor_enable01" {
+  provider_id = "okta_push"
+  active = "true"
+}
+
+resource "okta_factor" "factor_enable02" {
+  provider_id = "okta_sms"
+  active = "true"
+}
+
+resource "okta_factor" "factor_enable03" {
+  provider_id = "okta_call"
+  active = "true"
+}
+
+resource "okta_factor" "factor_enable04" {
+  provider_id = "google_otp"
+  active = "true"
+}
+
+#resource "okta_factor" "factor_enable05" {
+  #provider_id = "duo"
+  #active = "true"
+#}
+
+#resource "okta_factor" "factor_enable06" {
+ # provider_id = "fido_webauthn"
+ # active = "true"
+#}
