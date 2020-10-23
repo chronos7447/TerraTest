f5500c2ae307e91ecf7c2fccf0cde1a46232c857
chronos7447 <35457427+chronos7447@users.noreply.github.com>
2020-10-23 09:38:19 +0530

    Create AdminMFAPolicy.tf

---
+++ BaseModelSetup/AdminMFAPolicy.tf
@@ -0,0 +1,25 @@
+resource "okta_factor" "factor_enable" {
+  provider_id = ["okta_otp", "okta_push"]
+  active = "true"
+}
+
+resource "okta_policy_mfa" "MFAPolicy" {
+  name        = "Administrator"
+  status      = "ACTIVE"
+  description = "MFA Policy for Administrators "
+
+  okta_otp = {
+    enroll = "REQUIRED"
+  }
+  okta_push = {
+    enroll = "REQUIRED"
+  }
+  groups_included = [okta_group.CreateGroup.id]
+}
+
+resource "okta_policy_rule_mfa" "MFARule" {
+  policyid = join("",[okta_policy_mfa.MFAPolicy.id])
+  name = "Second Factor Enrollment Rule"
+  status = "ACTIVE"
+  enroll = "CHALLENGE" 
+}
