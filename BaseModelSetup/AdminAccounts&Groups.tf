6d15b352e29b5ca73d396173fc27fadd3624e625
chronos7447 <35457427+chronos7447@users.noreply.github.com>
2020-10-23 09:06:36 +0530

    Update AdminAccounts&Groups.tf

--- BaseModelSetup/AdminAccounts&Groups.tf
+++ BaseModelSetup/AdminAccounts&Groups.tf
@@ -6,3 +6,17 @@ resource "okta_user" "User01" {
   status    = "ACTIVE"
   password  = "George01"
 }
+
+resource "okta_user" "User02" {
+  email       = "AdminUser02@mail.com"
+  login       = "AdminUser02@mail.com"
+  first_name        = "Admin"
+  last_name = "User02"
+}
+
+resource "okta_user" "User03" {
+  email       = "AdminUser03@mail.com"
+  login       = "AdminUser03@mail.com"
+  first_name        = "Admin"
+  last_name = "User03"
+}
