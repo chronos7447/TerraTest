193fa25401b54503a34f6f8372ddf200ce196771
chronos7447 <35457427+chronos7447@users.noreply.github.com>
2020-10-23 12:09:19 +0530

    Create EmailTemp.tf

---
+++ BaseModelSetup/EmailTemp.tf
@@ -0,0 +1,15 @@
+resource "okta_template_email" "Temp01_ForgotPass" {
+  type = "email.forgotPassword"
+
+  translations {
+    language = "en"
+    subject  = "Stuff"
+    template = "Hi $${user.firstName},<br/><br/>We Will Reset Your Password $${resetPasswordLink}"
+  }
+
+  translations {
+    language = "es"
+    subject  = "Cosas"
+    template = "Hola $${user.firstName},<br/><br/>Password Change Link Amigo $${resetPasswordLink}"
+  }
+}
