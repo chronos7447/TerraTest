4975ccc9e5594dfefc48b4175b08d7fe9fb5b59a
chronos7447 <35457427+chronos7447@users.noreply.github.com>
2020-10-23 09:01:55 +0530

    Create AdminAccounts&Groups.tf

---
+++ AdminAccounts&Groups.tf
@@ -0,0 +1,10 @@
+resource "okta_user" "User01" {
+  email       = "AdminUser01@mail.com"
+  login       = "AdminUser01@mail.com"
+  first_name        = "Admin"
+  last_name = "User01"
+  status    = "ACTIVE"
+  password  = "George01"
+  master      = "OKTA"
+  scope       = "SELF"
+}
