cbdcc6ec03ed4e29f4d2706a694929c8bcf2e889
chronos7447 <35457427+chronos7447@users.noreply.github.com>
2020-10-23 11:15:16 +0530

    Update AdminSignOnPolicy.tf

--- BaseModelSetup/AdminSignOnPolicy.tf
+++ BaseModelSetup/AdminSignOnPolicy.tf
@@ -34,8 +34,8 @@ data "okta_policy" "Default" {
 resource "okta_policy_rule_signon" "SignOnRuleNetwork02" {
   policyid = join("",[data.okta_policy.Default.id])
   name = "Network Zone Restriction Rule "
-  #network_connection = "ZONE"
-  #network_includes = [] 
+  network_connection = "ZONE"
+  network_excludes = ["ALL_ZONES"] 
   access = "DENY"
   priority = "1"
 }
